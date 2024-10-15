f52c53eb082e2bda5d37717b2f52c02fef03f8cf
Jan Adamka <59932297+Jadamka@users.noreply.github.com>
2024-10-15 11:58:37 +0200

    Delete nvim directory

--- nvim/lua/plugins/rosepine-theme.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "rose-pine/neovim", name = "rose-pine",
-  config = function()
-        styles = { bold = true, italic = false, transparency = false },
-        vim.cmd [[colorscheme rose-pine]]
-  end
-}
