a36375f098c0bb9465af69c8a8546fb30d3e95a4
Jan Adamka <59932297+Jadamka@users.noreply.github.com>
2024-11-24 19:53:23 +0100

    best with font 12

---
+++ nvim/lua/plugins/rosepine-theme.lua
@@ -0,0 +1,7 @@
+return {
+  "rose-pine/neovim", name = "rose-pine",
+  config = function()
+        styles = { bold = true, italic = false, transparency = false }
+--        vim.cmd [[colorscheme rose-pine]]
+  end
+}
