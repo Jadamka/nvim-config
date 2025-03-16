8e8ae21fc1a3878f83bcda49046dd34c919556d4
Jan Adamka <59932297+Jadamka@users.noreply.github.com>
2025-03-16 18:25:54 +0100

    Add files via upload

---
+++ nvim/init.lua
@@ -0,0 +1,5 @@
+require("config.lazy")
+require("config.keymaps")
+
+vim.o.background = "dark"
+vim.cmd([[colorscheme doom-one]])
---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,6 @@
+-- clears the search
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
+-- nvim-tree
+vim.keymap.set('n', '<leader>op', ':NvimTreeFocus<CR>')
+
