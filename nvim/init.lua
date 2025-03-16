fca0abbc142f831afae140aa7c9840350627e4c6
Jan Adamka <59932297+Jadamka@users.noreply.github.com>
2025-03-16 18:25:17 +0100

    Delete nvim directory

--- nvim/init.lua
+++
@@ -1,5 +0,0 @@
-require("config.lazy")
-require("config.keymaps")
-
-vim.o.background = "dark"
-vim.cmd([[colorscheme doom-one]])
--- nvim/lua/config/keymaps.lua
+++
@@ -1,6 +0,0 @@
--- clears the search
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
-
--- nvim-tree
-vim.keymap.set('n', '<leader>op', ':NvimTreeFocus<CR>')
-
